a67a3a1af66dae9db78ec2cfcc44a3d4956c6b87
ttmousy <ttmousy@gmail.com>
2019-04-27 15:26:37 -0700

    update social links

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -2,8 +2,8 @@
 <div class="social">
     <ul>
         <li><a href="https://www.yelp.com/biz/a-list-makeup-milpitas-4?q=tam" target="_blank" class="fab fa-yelp"></a></li>
-        <li><a href="" target="_blank" class="fab fa-facebook-f"></a></li>
-        <li><a href="#" target="_blank" class="fab fa-instagram"></a></li>
+        <li><a href="https://www.facebook.com/dan.t.truong.7" target="_blank" class="fab fa-facebook-f"></a></li>
+        <li><a href="https://www.instagram.com/dantambeauty/" target="_blank" class="fab fa-instagram"></a></li>
         <li><a href="#" target="_blank" class="fab fa-pinterest"></a></li>
     </ul>
 </div>
